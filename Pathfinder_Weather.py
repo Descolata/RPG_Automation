a35f420069105ad950b2f831b6ae862bb7079e94
Descolata <119652031+Descolata@users.noreply.github.com>
2023-01-15 12:41:31 -0800

    Create Pathfinder_Weather.py

---
+++ Pathfinder_Weather.py
@@ -0,0 +1,10 @@
+"""
+Script runs Weather for Pathfinder, focusing on the Shackles but workable anywhere.
+"""
+import dice
+
+
+# Necessary variables
+distanceToEye = 500
+
+print( dice.roll('3d6') )
